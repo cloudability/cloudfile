efdc3aec27e4e00bcecf63192f9b371223f03a16
Carl Hall <carl@cloudability.com>
2013-10-22 13:03:43 -0700

    Change version of fog and excon to work with heroku-api better

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,7 +1,13 @@
 class cloudfile::dependencies {
-  package { "cloudfile_fog":
-    name => "fog",
-    ensure => hiera("fog_version"),
-    provider => "gem",
+  package {
+    "cloudfile_fog":
+      name => "fog",
+      ensure => hiera("fog_version"),
+      provider => "gem";
+
+    "cloudfile_excon":
+      name => "excon",
+      ensure => hiera("excon_version"),
+      provider => "gem";
   }
 }
